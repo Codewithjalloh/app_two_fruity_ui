c40fb3d56079e00e2c5dc4b90291725247f23856
codewithjalloh <codewithjalloh@gmail.com>
2024-05-09 15:50:20 +0100

    nav bar done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:app_two_fruity_ui/components/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
+
+import 'cart_page.dart';
+import 'shop_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +12,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // This selected index is to control the button nav bar
+  int _selectedIndex = 0;
+
+  // this method will update our selected index
+  // when the user taps on the botton nav bar
+  void navigatedButtomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages to display
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+
+    // cart page
+    const CartPage()
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder: (context) => IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.black,
+              )),
+        ),
+      ),
+      bottomNavigationBar: MyButtonNavBar(
+        onTabChange: (index) => navigatedButtomBar(index),
+      ),
+      drawer: Drawer(),
+    );
   }
 }
