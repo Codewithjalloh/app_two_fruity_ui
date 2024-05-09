18a9c9392252434d21985adc60112cae0b9355d5
codewithjalloh <codewithjalloh@gmail.com>
2024-05-09 16:04:21 +0100

    shop page added

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,7 +1,23 @@
+import 'package:app_two_fruity_ui/models/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ShopPage extends StatelessWidget {
+import '../models/fruit.dart';
+
+class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  // add fruit to cart
+  void addFruitToCart(Fruit fruit) {
+    Provider.of<Cart>(context, listen: false).addItemToCart(fruit);
+
+    // alert the user, fruit successfully added
+  }
 
   @override
   Widget build(BuildContext context) {
